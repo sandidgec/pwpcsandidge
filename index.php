777daf81d549ab6f48800865d1cfae91742b5885
Charles Sandidge <sandidgec@gmail.com>
2015-07-29 09:22:14 -0600

    added index, css and images

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+		<meta charset="UTF-8">
+		<title> Personal Website Project </title>
+	</head>
+	<body>
+		<div class="pageBorder">
+			<h1 class="headtext"> Design by &ltNinja&gt</h1>
+			<div class="navigation>"
+			<ul class="nav">
+				<li class="nav"><a href="index.php">Home</a></li>
+				<li><a href="contact.php">Contact</a></li>
+				<li><a href="about.php">About</a></li>
+			</ul>
+		</div>
+		<h2> Ninja Purpose  </h2>
+		<p> The purpose of my website is to offer a web presence for myself to showcase my web and graphic design
+			skills in a one-stop shop web experience.
+		</p>
+
+		<h2> Audience</h2>
+		<p> The audience for my portfolio website will be potential employers and clients who want to
+			explore content I have previously worked on and participated in. The website will showcase work in
+			a simplified and professional manner that will allow the user to dig deeper if they want to see
+			the depth of the work posted on the website and it will allow the casual site visitor a quick snap-shot
+			of the content as it relates to my professional skills and technical attributes.</p>
+		<h2> The Goal for &ltdesignByNinja&gt</h2>
+		<p>The main goal for this website is to serve as a professional business card that can be referenced to
+			when meeting potential clients or employers. The website will also serve a secondary goal in allowing me
+			to experiment with design ideas and coding techniques.
+		<div class="logo">
+			<img src="images/NinjaDesign.png" alt="Ninja Logo"
+		</div>
+		</p>
+		</div>
+	</body>
+</html>
+
