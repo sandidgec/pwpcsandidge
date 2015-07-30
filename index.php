6814284bd0c1e4310dfc8a1f5780a079678b1710
Charles Sandidge <sandidgec@gmail.com>
2015-07-29 19:34:09 -0600

    added sitemap, "the plan" and more

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 				<li class="nav"><a href="index.php">Home</a></li>
 				<li><a href="contact.php">Contact</a></li>
 				<li><a href="about.php">About</a></li>
+				<li><a href="portfolio.php">Portfolio</a> </li>
 			</ul>
 		</div>
 		<h2> Ninja Purpose  </h2>
@@ -22,15 +23,37 @@
 		</p>
 
 		<h2> Audience</h2>
-		<p> The audience for my portfolio website will be potential employers and clients who want to
+			<p> The audience for my portfolio website will be potential employers and clients who want to
 			explore content I have previously worked on and participated in. The website will showcase work in
 			a simplified and professional manner that will allow the user to dig deeper if they want to see
 			the depth of the work posted on the website and it will allow the casual site visitor a quick snap-shot
 			of the content as it relates to my professional skills and technical attributes.</p>
 		<h2> The Goal for &ltdesignByNinja&gt</h2>
-		<p>The main goal for this website is to serve as a professional business card that can be referenced to
+			<p>The main goal for this website is to serve as a professional business card that can be referenced to
 			when meeting potential clients or employers. The website will also serve a secondary goal in allowing me
 			to experiment with design ideas and coding techniques. </p>
+		<h2> User Persona </h2>
+			<p> Kevin (User Persona)- Kevin is the owner of a small web design firm and is looking to bring in another
+			employee to his growing company. He recently met Charles at a networking event and from his business card
+			got the address to his webpage with his portfolio on it. Wanting to see what kind of work Charles has done in
+			the past, Kevin visited the website and browsed the portfolio. </p>
+		<h2> Use Case</h2>
+			<ul>
+			<li> Kevin points his browser toward www.designByNinja.com</li>
+			<li> By viewing the clean design he is able to quickly navigate to the "Portfolio" section.</li>
+			<li> He browses the website samples in front of him and then Navigates to the contact page.</li>
+			<li> An easy to use form allows Kevin to send a message to Charles to give him a call to discuss
+			employment opportunities.</li>
+			</ul>
+		<h2> The Plan</h2>
+			<p>The plan for designByNinja.com is to layout and structure the site in an easy navigate style that also
+		showcases some cool designs and "tricks". Then the site will be populated with portfolio worthy designs and
+		links to outside work.</p>
+
+		<h2> SiteMap</h2>
+		<img src="images/sitemapdesignbyninja.png" alt="Sitemap>"
+
+
 		<div class="logo">
 			<img src="images/NinjaDesign.png" alt="Ninja Logo"
 		</div>
