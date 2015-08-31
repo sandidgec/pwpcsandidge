ea20ef7fd853dd799b2d0dd118d246f3e5b377b7
Charles Sandidge <sandidgec@gmail.com>
2015-08-31 16:40:18 -0600

    added pages, images and front logo

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-	<h1> portfolio</h1>
+	<head>
+		<?php require_once ("../php/head-utils.php"); ?>
+
+		<link type="text/css" href="../lib/css/mystyle.css" rel="stylesheet"/>
+	</head>
+	<body class="site">
+		<div class="site-content">
+			<header>
+				<?php require_once ("../php/header.php"); ?>
+			</header>
+
+
+			<footer>
+				<?php require_once("../php/footer.php"); ?>
+			</footer>
+	</body>
 </html>
